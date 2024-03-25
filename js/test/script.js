73971b5bbc78e175cf8815bf978c39eafe590b54
Anuraj Kumar <anuraj.business@gmail.com>
2024-03-26 02:44:44 +0530

    removing files

---
+++ js/test/script.js
@@ -0,0 +1,12 @@
+// script.js
+function checkPassword() {
+    const enteredPassword = document.getElementById('password').value;
+    const secretPassword = 'mysecret'; // Change this to your desired password
+
+    if (enteredPassword === secretPassword) {
+        document.getElementById('hidden-content').style.display = 'block';
+    } else {
+        alert('Incorrect password. Try again.');
+    }
+}
+
